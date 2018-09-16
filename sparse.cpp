f5607f8076f04a799ad771886b723197106db42c
bLACKZU <satyakighosh65@gmail.com>
2018-09-17 01:36:50 +0530

    WIP

--- sparse.cpp
+++ sparse.cpp
@@ -3,28 +3,32 @@
 int **createMat(int, int);
 int inputMat(int**, int, int);
 void **sparse(int**, int**, int, int, int, int);
+void display(int**, int, int);
 void delMat(int**, int);
+
 
 int main()
 {
-	int **arr = nullptr, **sparseMat = nullptr, rows, columns, nonZeroCount = 0;
+	int **arr = nullptr, **sparseMat = nullptr, **resultSp = nullptr, rows, columns, nonZeroCount = 0;
 	int consCol = 3;
 	std::cout << "Enter the number of rows and columns" << std::endl;
 	std::cin >> rows >> columns;
 	arr = createMat(rows, columns);
-	std::cout << "Enter the number of elements in the matrix" << std::endl;
-	inputMat(arr, rows, columns);
+	std::cout << "Enter the elements in the matrix" << std::endl;
 	nonZeroCount = inputMat(arr, rows, columns);
-	sparseMat = createMat(nonZeroCount, consCol);
-	sparse(sparseMat, arr, nonZeroCount, rows, columns, nonZeroCount); 
-	delMat(arr, rows);
+	sparseMat = createMat(nonZeroCount + 1, consCol);
+	resultSp = sparse(sparseMat, arr, nonZeroCount, rows, columns, nonZeroCount); 
+	std::cout << "The COO representation of the sparse-matrix is" << std::endl;
+	display(resultSp, nonZeroCount, consCol);
 	delMat(sparseMat, nonZeroCount); 
+	delMat(arr, rows);
 }
+
 int **createMat(int row, int col)
 {
 	int **arrp = new int*[row];
 	for(int i = 0; i < row; i++)
-		*(arrp + i) = new int[col];
+		*(arrp + i) = new int[col]();
 	return arrp;
 }
 
@@ -43,41 +47,54 @@ int inputMat(int **arr, int row, int col)
 	return nonZeroCount;
 }
 
-void **sparse(int **sparseMat, int **arr, int spRow, int row, int col, int nonZeroCount)
+int **sparse(int **sparseMat, int **arr, int spRow, int row, int col)
 {
-	int z, x, bak = 0;
+	int z = 0;
+	int x = 0;
+	int y = 0;
 	sparseMat[0][0] = row;
 	sparseMat[0][1] = col;
-	sparseMat[0][2] = nonZeroCount;
-	for(z = 0; z < spRow; z++)
+	sparseMat[0][2] = spRow;
+	for(z = 0, y = 1; y <= spRow + 1; )
 	{
-		bak = z;
-		if(x < col && *(*(arr + z) + x) != 0)
+		if(x < col && *(*(arr + z) + x) != 0))
 		{
-			sparseMat[z + 1][0] = z;
-			sparseMat[z + 1][1] = x;
-			sparseMat[z + 1][2] = *(*(arr + z) + x);
+			sparseMat[y][0] = z;
+			sparseMat[y][1] = x;
+			sparseMat[y][2] = *(*(arr + z) + x);
 			x++;
-			z = bak;
+			y++;
 			continue;
 		}
-		else if(x < col && *(*(arr + z) + x) == 0)
+		else if(x < col && *(*(arr + z) + x) == 0))
 		{
 			x++;
-			z = bak;
 			continue;
 		}
 		else if(x == col)
 		{
 			x = 0;
+			z++;
 			continue;
 		}	
-	}	
+	}
+	return sparseMat;
 }			
+
+void display(int **sparseMat, int spRow, int spCol)
+{
+	for(int i = 0; i <= spRow; i++)
+	{
+		for(int j = 0; j < spCol; j++)
+			std::cout << sparseMat[i][j] << " ";
+		std::cout << "\n";
+	}
+}
 
 void delMat(int **arr, int row)
 {
 	for(int i = 0; i < row; i++)
 		delete [] arr[i];
 	delete [] arr;
-}	
+}
+	
